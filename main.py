7dbc330c3f3ba9d54a6b35fcaa340727b752f8f0
Yong <finetopix@gmail.com>
2024-05-16 12:37:03 +1000

    merge mapinfo tab files remove duplicates based on transmitter

--- main.py
+++ main.py
@@ -4,10 +4,37 @@ import os
 import geopandas as gpd
 import pandas as pd
 
+from tkinter import Tk, messagebox  # For Python 2.x, use 'Tkinter' instead
+from tkinter.filedialog import askopenfilename
+
+
+
+
+def extract_zone(prediction_name):
+    # zone number is always the last two characters
+    return prediction_name[-2:]
+
+if __name__ == "__main__":
+    Tk().withdraw()
+    file_path = 'SiteSystemsByPostcodeSuburb_20240411.xlsx'
+    file_path = askopenfilename()
+    zones = [50,51,52,53,54,55,56]
+file_path = r"C:\Users\T1603086\Downloads"
 # Load the first TAB file
 file1 = r"C:\Users\T1603086\Downloads\z50\LTE_OD_50.tab"
 data1 = gpd.read_file(file1)
-print(data1.columns)
+# columns:LEGEND,THRESHOLD,COLOR,Prediction_name,Transmitter,Site,Site_UTMZONE
+# remove transmitter which UTMZONE is not equal to zone in Prediction_name(like LTE_OD_56)
+# Apply the function to create a new column with the extracted zone
+data1['Extracted_Zone'] = data1['Prediction_name'].apply(extract_zone)
+
+# Filter the DataFrame to keep only the rows where 'Site_UTMZONE' matches 'Extracted_Zone'
+data1_filtered = data1[data1['Site_UTMZONE'] == data1['Extracted_Zone']]
+
+# Drop the 'Extracted_Zone' column as it's no longer needed
+data1_filtered = data1_filtered.drop(columns=['Extracted_Zone'])
+
+
 # Load the second TAB file
 file2 = r"C:\Users\T1603086\Downloads\z51\LTE_OD_51.tab"
 data2 = gpd.read_file(file2)
