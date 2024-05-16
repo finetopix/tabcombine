99aab78e21382915c9c581bb518fdbb4bbf1af3f
Yong <finetopix@gmail.com>
2024-05-16 14:45:06 +1000

    remove duplicates when zone is not the actual zone zone from prediction_name vs actual zone Site.UTMZONE

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import os
-#import fiona
-#print('MapInfo File' in fiona.supported_drivers)  # Should print True if ready
 import geopandas as gpd
 import pandas as pd
-
 from tkinter import Tk, messagebox  # For Python 2.x, use 'Tkinter' instead
-from tkinter.filedialog import askopenfilename
-
-
+from tkinter import filedialog
 
 
 def extract_zone(prediction_name):
@@ -15,50 +10,41 @@ def extract_zone(prediction_name):
     return prediction_name[-2:]
 
 if __name__ == "__main__":
-    Tk().withdraw()
-    file_path = 'SiteSystemsByPostcodeSuburb_20240411.xlsx'
-    file_path = askopenfilename()
+    Tk().withdraw() # Hide the main window
+    #file_path = filedialog.askopenfilename()
+    folder_path = filedialog.askdirectory()
+    predictions = [
+        "LTE_OD_",
+        "LTE_ID_",
+        "LTE_HB_OD_",
+        "LTE_HB_ID_",
+        "NR_OD_",
+        "NR_ID_",
+        "NR_C_OD_",
+        "NR_C_ID_"
+    ]
     zones = [50,51,52,53,54,55,56]
-file_path = r"C:\Users\T1603086\Downloads"
-# Load the first TAB file
-file1 = r"C:\Users\T1603086\Downloads\z50\LTE_OD_50.tab"
-data1 = gpd.read_file(file1)
-# columns:LEGEND,THRESHOLD,COLOR,Prediction_name,Transmitter,Site,Site_UTMZONE
-# remove transmitter which UTMZONE is not equal to zone in Prediction_name(like LTE_OD_56)
-# Apply the function to create a new column with the extracted zone
-data1['Extracted_Zone'] = data1['Prediction_name'].apply(extract_zone)
-
-# Filter the DataFrame to keep only the rows where 'Site_UTMZONE' matches 'Extracted_Zone'
-data1_filtered = data1[data1['Site_UTMZONE'] == data1['Extracted_Zone']]
-
-# Drop the 'Extracted_Zone' column as it's no longer needed
-data1_filtered = data1_filtered.drop(columns=['Extracted_Zone'])
-
-
-# Load the second TAB file
-file2 = r"C:\Users\T1603086\Downloads\z51\LTE_OD_51.tab"
-data2 = gpd.read_file(file2)
-
-file3 = r"C:\Users\T1603086\Downloads\z52\LTE_OD_52.tab"
-data3 = gpd.read_file(file3)
-file4 = r"C:\Users\T1603086\Downloads\z53\LTE_OD_53.tab"
-data4 = gpd.read_file(file4)
-file5 = r"C:\Users\T1603086\Downloads\z54\LTE_OD_54.tab"
-data5 = gpd.read_file(file5)
-file6 = r"C:\Users\T1603086\Downloads\z55\LTE_OD_55.tab"
-data6 = gpd.read_file(file6)
-file7 = r"C:\Users\T1603086\Downloads\z56\LTE_OD_56.tab"
-data7 = gpd.read_file(file7)
-
-# Combine the two datasets and remove duplicates
-combined_data = gpd.GeoDataFrame(pd.concat([data1, data2, data3, data4, data5, data6, data7], ignore_index=True),crs=data1.crs)
-combined_data = combined_data.drop_duplicates(subset='Transmitter') # remove duplicates based on transmitter
-
-
-# Save the combined dataset to a new TAB file
-os.chdir(r"C:\Users\T1603086\Downloads")
-output_file = "tpg_combined_file-removedup.tab"
-
-combined_data.to_file(output_file, driver="MapInfo File")
-
-print("Combined file saved successfully.")
+    combined_data = pd.DataFrame()
+    for prediction in predictions:
+        for zone in zones:
+            file_name = prediction + str(zone) + '.tab'
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.exists(file_path):
+                data = gpd.read_file(file_path)
+                # columns:LEGEND,THRESHOLD,COLOR,Prediction_name,Transmitter,Site,Site_UTMZONE
+                # remove transmitter which UTMZONE is not equal to zone in Prediction_name(like LTE_OD_56)
+                # Apply the function to create a new column with the extracted zone
+                data['Extracted_Zone'] = data['Prediction_name'].apply(extract_zone)
+                # Filter the DataFrame to keep only the rows where 'Site_UTMZONE' matches 'Extracted_Zone'
+                data_filtered = data[data['Site_UTMZONE'] == data['Extracted_Zone']]
+                # Drop the 'Extracted_Zone' column as it's no longer needed
+                data_filtered = data_filtered.drop(columns=['Extracted_Zone'])
+                # Combine the two datasets and remove duplicates
+                combined_data = gpd.GeoDataFrame(pd.concat([combined_data,data_filtered], ignore_index=True))
+                # combined_data = combined_data.drop_duplicates(subset='Transmitter') # remove duplicates based on transmitter
+        # Save the combined dataset to a new TAB file
+        output_filename = prediction + "AllSites.tab"
+        output_file = os.path.join(folder_path,output_filename)
+        combined_data.to_file(output_file, driver="MapInfo File")
+        print("Combined file saved successfully:",output_file)
+    print("All files combined!")
